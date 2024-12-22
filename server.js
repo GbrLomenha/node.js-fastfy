c2dd71b54fd84f40c53971c0f3fdd2b69c975701
Gabriel da Silva Lomenha <gabriellomenha@gmail.com>
2024-12-22 12:39:43 -0300

    instalação do prisma dotenv nodemon e prismaclient

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
 import fastify from 'fastify';
+import { PrismaClient } from '@prisma/client';
 
+const prisma = new PrismaClient();
 const app = fastify();
+
+app.get('/usuario/listar', async (request, reply) => {
+    const users = await prisma.usuario.findMany({
+        select: {
+            name: true,
+            email: true
+        }
+    });
+    return users;
+})
 
 app.listen({ port: 3000 }, (err, address) => {
     if (err) console.log(err)
